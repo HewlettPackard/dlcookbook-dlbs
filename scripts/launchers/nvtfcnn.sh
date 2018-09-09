f2151ae9ac4d93f1784e57400ff9265f73a552ac
sfleisch <stephen.fleischman@hpe.com>
2018-09-08 19:17:40 -0500

    Had to rearrange code a bit so now single node works with Docker

--- scripts/launchers/nvtfcnn.sh
+++ scripts/launchers/nvtfcnn.sh
@@ -19,19 +19,31 @@ fi
 if [ "${exp_docker}" = "true" ]; then
     assert_docker_img_exists ${exp_docker_image}
     export run_container="${exp_docker_launcher} run ${tensorflow_docker_args}"
+    # Won't work for multi-host. Will require some work.
+    # https://github.com/NVIDIA/nvidia-docker/wiki/Frequently-Asked-Questions#do-you-support-docker-compose:
+    script="\
+        export ${tensorflow_env};\
+        echo -e \"__results.start_time__= \x22\$(date +%Y-%m-%d:%H:%M:%S:%3N)\x22\";\
+        ${runtime_launcher}  ${run_container} ${exp_mpirun} ${exp_mpirun_args} ${runtime_python} ${tensorflow_python_path}/benchmarks.py ${nvtfcnn_args};\
+        echo -e \"__results.end_time__= \x22\$(date +%Y-%m-%d:%H:%M:%S:%3N)\x22\";
+"
 elif [ "${exp_singularity}" = "true" ]; then
     assert_singularity_img_exists ${exp_singularity_image}
     export run_container="${exp_singularity_launcher} exec ${tensorflow_singularity_args}"
+    script="\
+        export ${tensorflow_env};\
+        echo -e \"__results.start_time__= \x22\$(date +%Y-%m-%d:%H:%M:%S:%3N)\x22\";\
+        ${runtime_launcher}  ${exp_mpirun} ${exp_mpirun_args} ${run_container} ${runtime_python} ${tensorflow_python_path}/benchmarks.py ${nvtfcnn_args};\
+        echo -e \"__results.end_time__= \x22\$(date +%Y-%m-%d:%H:%M:%S:%3N)\x22\";
+    "
 else
-    export run_container=""
+    script="\
+        export ${tensorflow_env};\
+        echo -e \"__results.start_time__= \x22\$(date +%Y-%m-%d:%H:%M:%S:%3N)\x22\";\
+        ${runtime_launcher}  ${exp_mpirun} ${exp_mpirun_args} ${runtime_python} ${tensorflow_python_path}/benchmarks.py ${nvtfcnn_args};\
+        echo -e \"__results.end_time__= \x22\$(date +%Y-%m-%d:%H:%M:%S:%3N)\x22\";
+    "
 fi
-
-script="\
-    export ${tensorflow_env};\
-    echo -e \"__results.start_time__= \x22\$(date +%Y-%m-%d:%H:%M:%S:%3N)\x22\";\
-    ${runtime_launcher}  ${exp_mpirun} ${exp_mpirun_args} ${run_container} ${runtime_python} ${tensorflow_python_path}/benchmarks.py ${nvtfcnn_args};\
-    echo -e \"__results.end_time__= \x22\$(date +%Y-%m-%d:%H:%M:%S:%3N)\x22\";
-"
 echo script $script
 /bin/bash -c "eval $script" >> ${exp_log_file} 2>&1
 
