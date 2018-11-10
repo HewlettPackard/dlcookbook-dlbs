f2e59efedd535ef7baa27812b22e4d70c27d6500
Stephen Fleischman <stephen.fleischman@hpe.com>
2018-11-09 19:42:35 -0600

    benchmarks.py: bug fix, yet again, on handling models like resnet and vgg

--- python/nvtfcnn_benchmarks/benchmarks.py
+++ python/nvtfcnn_benchmarks/benchmarks.py
@@ -22,27 +22,28 @@ import sys
 import re
 
 from subprocess import call
+models_re=['alexnet$','inception_resnet_v2$','inception_v4$',r'resnet_\d+$',r'vgg_\d+$',
+            'googlenet$','inception_v3$','overfeat$','trivial$','xception$']
+def allowed_models(v):
+    v=v.strip()
+    for model_re in models_re:
+        if re.match(model_re,v): break
+    else:
+        raise argparse.ArgumentTypeError('The specified model {} does not match any recognized pattern ({})'.format(v,','.join(models_re)))
+    return v
 
 def main():
     """Main worker function."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--model', type=str, required=True, default='',
-        help="A model to benchmark ('alexnet', 'googlenet' ...)"
-    )
-
     parser = argparse.ArgumentParser(add_help=False)
     #Update here if list of available models change.
-    models=['alexnet','inception_resnet_v2','inception_v4','resnet','vgg',
-            'googlenet','inception_v3','overfeat','trivial','xception']
-    parser.add_argument( '--model', type=str, required=True,
-                         default='', choices=models,
-                         help="A model to benchmark ({})".format(', '.join(models)))
+    parser.add_argument( '--model', type=allowed_models, required=True,
+                         default='', 
+                         help="A model to benchmark - must match one of the patterns: {}".format(' | '.join(models_re)))
     args, passthru = parser.parse_known_args()
     m=re.match("(resnet|vgg)_(\d+)",args.model)
     if m:
         args.model=m.group(1)
-        passthru+=" --layers {}".format(m.group(2))
+        passthru+=["--layers {}".format(m.group(2))]
 
     prog=pathlib.Path(__file__).parent.joinpath('cnn').joinpath(args.model+".py")
     cmd=['python',str(prog)] + passthru
