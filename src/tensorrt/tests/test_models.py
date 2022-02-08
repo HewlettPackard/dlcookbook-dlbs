834350c81154e48af132b7d27874e30a7b80a78c
sergey-serebryakov <serebryakov.sergey@gmail.com>
2022-02-08 14:37:50 -0800

    Updting TensorRT model test code.
    - Adding better docstrings.

--- src/tensorrt/tests/test_models.py
+++ src/tensorrt/tests/test_models.py
@@ -1,11 +1,12 @@
+"""Python-based tools to test TensorRT models in *.prototxt and *.onnx formats."""
 import os
 import sys
 import shutil
-import mlflow
 import logging
 import tempfile
-import typing as t
 import subprocess
+import typing as t
+import mlflow
 from dlbs.bench_data import BenchDataApp
 
 
@@ -13,13 +14,21 @@ logger = logging.getLogger()
 
 
 class BenchmarkStatus:
-    """ Holder for benchmark statuses. """
+    """Holder for benchmark statuses."""
+
     FAILURE: int = 0
     SUCCESS: int = 1
 
 
 def get_benchmark_status(benchmark: t.Union[str, t.Dict]) -> int:
-    """ Return status ('success' or 'failure') of a benchmark. """
+    """Return status ('success' or 'failure') of a benchmark.
+
+    Args:
+        benchmark (t.Union[str, t.Dict]): A dictionary with benchmark results, or a path to a DLBS log file.
+
+    Returns:
+        int: benchmark status: :py:attr:`BenchmarkStatus.SUCCESS` or :py:attr:`BenchmarkStatus.FAILURE`
+    """
     if isinstance(benchmark, str):
         # If it's a string, consider this to be a path to a log file.
         benchmarks: t.List[t.Dict] = BenchDataApp(
@@ -44,13 +53,25 @@ def get_benchmark_status(benchmark: t.Union[str, t.Dict]) -> int:
 
 
 def get_tensorrt_exec() -> str:
-    """ Return executable for DLBS TensorRT inference benchmarks. """
+    """Return executable for DLBS TensorRT inference benchmarks.
+
+    Returns:
+        str: Path to a tensorrt executable, or default value (tensorrt). `DLBS_TENSORRT_EXEC` environmen variable
+            is checked for a value provided by a user.
+    """
     tensorrt_exec: str = os.environ.get('DLBS_TENSORRT_EXEC', None)
     return tensorrt_exec or 'tensorrt'
 
 
 def get_mlflow_artifact_uri() -> str:
-    """ Return MLFlow artifact URI making sure it's a local file system. """
+    """Return MLFlow artifact URI making sure it's a local file system.
+
+    Raises:
+        ValueError: If artifact store is accessible by a protocol other than `file://`.
+
+    Returns:
+        str: A path to file store with a protocol prefix, e.g. `/storage/artifact_store`.
+    """
     artifact_uri: str = mlflow.get_artifact_uri()
     if not artifact_uri.startswith('file://'):
         raise ValueError(f"Unsupported artifact URI protocol (artifact_uri = {artifact_uri})")
@@ -58,11 +79,18 @@ def get_mlflow_artifact_uri() -> str:
 
 
 class CafeModels:
-    """ Helper functions to work with DLBS Cafe models. """
+    """Helper functions to work with DLBS Cafe models."""
 
     @staticmethod
     def get_model_title(file_path: str) -> t.Optional[str]:
-        """ Open model file and find its title if present. """
+        """Open model file and find its title if present.
+
+        Args:
+            file_path (str): Path to a file in prototxt format defining a NN model in Caffe format.
+
+        Returns:
+            t.Optional[str]: A model name, or None if not found.
+        """
         with open(file_path, 'r') as config:
             for line in config:
                 if line.startswith('name:'):
@@ -71,7 +99,22 @@ class CafeModels:
 
     @staticmethod
     def init_cafe_file(model: t.Mapping, params: t.Mapping) -> t.Dict:
-        """ Prepare Cafe training/inference file for a benchmark. """
+        """Prepare Cafe training/inference file for a benchmark.
+
+        This method is required because DLBS stores Caffe models in prototxt format that are not ready to be consumed
+        by the TensorRT's caffe parser.
+
+        Args:
+            model (t.Mapping): Model specification, a dictionary returned by the :py:method:`CafeModels.get_models`
+                method.
+            params (t.Mapping): Parameters of a target file. The following fields must be present: `path` path to a
+                directory, `phase` model phase (`training` or `inference`), `batch_size` size of a replica batch.
+
+        Returns:
+            t.Dict: Parameters of a model ready to be consumed by the DLBS TensorRT benchmark backend. Contains the
+                following fields: `model_path` full path to a model file, `model_dir` model directory, `model_file`
+                model file name, `phase` model phase (`training`/`inference`).
+        """
         target_dir: str = params['path']
         phase: str = params['phase']
         target_file: str = model[phase]['alias'] + '.prototxt'
@@ -90,7 +133,15 @@ class CafeModels:
 
     @staticmethod
     def get_models(path: str) -> t.List[t.Dict]:
-        """ Get all supported models recursively. """
+        """Get all supported models recursively.
+
+        Args:
+            path (str): Directory path.
+
+        Returns:
+            t.List[t.Dict]: List of dictionaries each containing a model description. Directory is traversed
+                recursively, and all supported models (*.prototxt and *.onnx) are returned.
+        """
         models: t.List[t.Dict] = []
         for root, _, files in os.walk(path):
             for file in files:
@@ -106,12 +157,23 @@ class CafeModels:
         return models
 
     def __init__(self, source_dir: t.Optional[str] = None) -> None:
+        """Initialize this class by assigning the path to DLBS Caffe models in prototxt format.
+
+        Args:
+            source_dir (t.Optional[str], optional): A path to DLBS Caffe models in prototxt format. If none,
+                 this method will try to guess the path. Defaults to None.
+        """
         if source_dir is None:
             source_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../models'))
         self.source_dir = source_dir
 
     def get_standard_models(self) -> t.List[t.Mapping]:
-        """ Get information on all DLBS Cafe models. """
+        """Get information on all DLBS Cafe models.
+
+        Returns:
+            t.List[t.Mapping]: List of dictionaries describing standard DLBS Caffe models in prototxt format. These
+                models are historically located in `${DLBS_ROOT}/models` directory.
+        """
         models: t.List[t.Mapping] = []
         for model_dir in (d for d in os.scandir(self.source_dir) if d.is_dir()):
             model = dict(model_dir=model_dir.path, name=model_dir.name)
@@ -131,7 +193,7 @@ class CafeModels:
 
 
 def test_dlbs_caffe_models() -> None:
-    """ Test standard DLBS Caffe (prototxt) models. """
+    """Test standard DLBS Caffe (prototxt) models."""
     params = dict(batch_size=1, gpus='0', dtype='float32', num_warmup_batches=1, num_batches=1,
                   tensorrt_exec=get_tensorrt_exec())
     models_dir = tempfile.mkdtemp()
@@ -145,7 +207,8 @@ def test_dlbs_caffe_models() -> None:
 
 
 def test_models() -> None:
-    """ Test onnx (or other - prototxt) models.
+    """Test onnx (or other - prototxt) models.
+
     One possible source of models is the ONNX models repository here: https://github.com/onnx/models
     """
     params = dict(batch_size=1, gpus='0', dtype='float32', num_warmup_batches=1, num_batches=1,
@@ -161,9 +224,16 @@ def test_models() -> None:
 def run_tests(run_name: str, tags: t.Optional[t.Mapping],
               models_dir: str, tensorrt_exec: t.Optional[str],
               params: t.Optional[t.Mapping]) -> None:
-    """ Main test function.
-    Inputs: Models directory (artifact), Path to TensorRT file (artifact), benchmark parameters.
-    Outputs: An artifact of type 'DLBSBenchmarkResult'.
+    """Run model and inference tests using MLFlow as a tracking service.
+
+    Args:
+        run_name (str): MLFlow name of this run.
+        tags (t.Optional[t.Mapping]): Dictionary of tags to log with MLFlow.
+        models_dir (str): Path to models to test. Models are searched recursively, and all supported models (*.prototxt
+            and *.onnx) are used in these tests.
+        tensorrt_exec (t.Optional[str]): Path to DLBS TensorRT benchmark backend executable.
+        params (t.Optional[t.Mapping]): Dictionary of benchmark parameters that include `batch_size`, `gpus`,
+            `dtype`, `num_warmup_batches` and `num_batches`. These are parameters accepted by TensorRT benchmark tool.
     """
     with mlflow.start_run(run_name=run_name, tags=tags):
         tags = dict(dict(type='dlbs', backend='tensorrt'), **(tags or {}))
@@ -206,15 +276,17 @@ def run_tests(run_name: str, tags: t.Optional[t.Mapping],
 
 
 def main():
-    """ Run inference tests with TensorRT DLBS benchmark backend.
+    """Run inference tests with TensorRT DLBS benchmark backend.
 
     Useful environment variables:
         MLFLOW_TRACKING_URI, DLBS_TENSORRT_EXEC, DLBS_TEST_MODELS
     Example usage:
+        ```
         $ export MLFLOW_TRACKING_URI=${HOME}/.mlflow
         $ export DLBS_TENSORRT_EXEC=${HOME}/projects/dlbs/build/tensorrt
         $ DLBS_TEST_MODELS= python ./tests/test_models.py dlbs
         $ DLBS_TEST_MODELS=${HOME}/projects/onnx_models/vision/classification/resnet/model python ./tests/test_models.py
+        ```
     """
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     if not os.environ.get('DLBS_TEST_MODELS', None):
